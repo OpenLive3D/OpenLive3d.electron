ef3bbd197ac254f20e503a0d9f8ad0478e14e635
Chen Wei <weichen@apache.org>
2023-04-10 02:03:40 +0100

    2023-04-10-beta-1.3.1

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
 const electron = require('electron');
 
-const { readFileSync, existsSync, writeFileSync } = require('fs');
 const { app } = electron;
 const { BrowserWindow } = electron;
 const { ipcMain } = electron;
+const {
+    readFileSync,
+    existsSync,
+    writeFileSync,
+    mkdirSync
+} = require('fs');
 
 const path = require('path');
 const url = require('url');
 
+function getAppDataPath() {
+    switch (process.platform) {
+        case "darwin": {
+            return path.join(process.env.HOME, "Library", "Application Support", "OpenLive3D");
+        }
+        case "win32": {
+            return path.join(process.env.APPDATA, "OpenLive3D");
+        }
+        case "linux": {
+            return path.join(process.env.HOME, ".OpenLive3D");
+        }
+        default: {
+            console.log("Unsupported platform!");
+            process.exit(1);
+        }
+    }
+}
+
+const appDatatDirPath = getAppDataPath();
+if (!existsSync(appDatatDirPath)) {
+    mkdirSync(appDatatDirPath);
+}
+const appDataFilePath = path.join(appDatatDirPath, 'config.json');
 function readConfig() {
-    if(existsSync('./config.json')){
-        return readFileSync('./config.json', 'utf8');
+    if(existsSync(appDataFilePath)){
+        return readFileSync(appDataFilePath, 'utf8');
     }else{
         return null;
     }
 }
 function saveConfig(saveString) {
-    writeFileSync('./config.json', saveString);
+    writeFileSync(appDataFilePath, saveString);
 }
 
 let win;
@@ -32,7 +60,6 @@ function createWindow() {
             backgroundThrottling: false,
         },
     });
-    win.webContents.openDevTools();
 
     // and load the index.html of the app.
     win.loadURL(url.format({
